b6f4d33b4c650672c52eb440de54edf3fbd6ea4a
Tony Bowden <tony@tmtm.com>
2019-02-07 14:00:52 +0000

    Gather data and then save at end

--- scraper.rb
+++ scraper.rb
@@ -19,16 +19,16 @@ def idify(a)
   name.tr(' ', '-').downcase
 end
 
-def scrape(url)
+def member_list(url)
   noko = noko_for(url)
 
   constituency = ''
-  noko.xpath('.//h2[contains(.,"Current members")]/following-sibling::table[1]//tr[td]').each do |tr|
+  noko.xpath('.//h2[contains(.,"Current members")]/following-sibling::table[1]//tr[td]').map do |tr|
     tds = tr.css('td')
     constituency = tds.shift if tds.count == 3
 
     next if tds[0].text.strip == 'Vacant'
-    data = {
+    {
       id:            idify(tds[0].css('a')),
       name:          tds[0].text.tidy,
       wikipedia__en: tds[0].xpath('a[not(@class="new")]/@title').text.tidy,
@@ -37,9 +37,11 @@ def scrape(url)
       term:          '2011',
       source:        url,
     }
-    ScraperWiki.save_sqlite(%i(id term), data)
   end
 end
 
+data = member_list('https://en.wikipedia.org/w/index.php?title=House_of_Keys&oldid=743685692').compact
+data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
+
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
-scrape('https://en.wikipedia.org/w/index.php?title=House_of_Keys&oldid=743685692')
+ScraperWiki.save_sqlite(%i(id term), data)
